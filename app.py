98585e04dbf37149698b9f59fe1fe604622e7b34
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2017-03-04 19:46:49 +0700

    Create resolver

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 
 from userapi import userapi
 from urlapi import urlapi
+from resolver import resolver
 
 from util import local_make_response
 
 app = Flask(__name__)
 app.register_blueprint(userapi, url_prefix = '/user')
 app.register_blueprint(urlapi, url_prefix = '/url')
+app.register_blueprint(resolver, url_prefix = '/s')
 
 @app.errorhandler(404)
 def error404(e):
---
+++ resolver.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+from flask import redirect
+
+from dbhandler import DBHandler
+from dbhandler import DBException
+
+resolver = Blueprint('resolver', __name__)
+
+@resolver.route('/')
+def index():
+    return "Invalid request"
+
+@resolver.route('/<short>')
+def resolve(short):
+    db = DBHandler()
+
+    try:
+        redir = db.urlLookup(short)
+    except DBException as err:
+        return err.message
+    else:
+        return redirect(redir)
