39e5f5be81d8df37db94a680ed32f2978880284e
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2017-03-04 11:18:22 +0700

    delete garbage

--- api.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from dbhandler import DBHandler
-from util import local_make_response
-
-userapi = Blueprint('userapi', __name__)
-
-@api.route('/register', methods = ['POST'])
