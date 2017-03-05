3fda5b23554dfdb8a9ce9324c7c878b009c74ad9
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2017-03-05 07:54:47 +0700

    Fix database primary key bug

--- database.sql
+++ database.sql
@@ -39,7 +39,7 @@ DROP TABLE IF EXISTS `own`;
 CREATE TABLE `own` (
   `username` varchar(32) NOT NULL,
   `id` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`username`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
